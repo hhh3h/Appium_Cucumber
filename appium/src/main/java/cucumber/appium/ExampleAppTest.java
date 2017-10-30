ffc4185df82d37304617cd6e2b6ac331dac00c34
hyunhwa.hong <781202@hanmail.net>
2017-10-30 23:48:19 +0900

    added sikuli test in 샘플앱테스트.feature

--- appium/src/main/java/cucumber/appium/ExampleAppTest.java
+++ appium/src/main/java/cucumber/appium/ExampleAppTest.java
@@ -3,7 +3,9 @@ package cucumber.appium;
 import cucumber.appium.AppSetting;
 import cucumber.appium.Property;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.sikuli.script.*;
 
 public class ExampleAppTest {
 	private StringBuffer verificationErrors;
@@ -19,8 +21,23 @@ public class ExampleAppTest {
 
 	@Given("^\"([^\"]*)\" 을 실행 시킨다\\.$")
 	public void 을_실행_시킨다(String serviceName) throws Throwable {
-	    // Not yet ...
 	    return;
+	}
+	
+	// sikuli을 이용한 첫화면 검증을 시도 한다.
+	@Then("^\"([^\"]*)\" 의 첫화면을 검증 한다\\.$")
+	public void 의_첫화면을_검증_한다(String arg1) throws Throwable {
+		Screen sn = new Screen();
+        try{
+        		sn.capture(sn.getBounds());
+                sn.wait("src/img/exam_main.png");
+                sn.click("src/img/exam_1.png", 0);
+                sn.wait("src/img/exam_1_green.png");
+                //sn.type(null, "hello world\n", 0);
+        }
+        catch(FindFailed e){
+                e.printStackTrace();
+        }
 	}
 
 }
