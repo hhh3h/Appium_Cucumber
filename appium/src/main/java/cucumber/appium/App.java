1a9bb90870fd8336930f819ebab7c5bf706051e2
hyunhwa.hong <781202@hanmail.net>
2017-11-05 14:57:43 +0900

    added OCR.java for image recognition

--- appium/src/main/java/cucumber/appium/App.java
+++ appium/src/main/java/cucumber/appium/App.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= {"target"},tags={"@Cucumber"})
+@CucumberOptions(features= {"target"},tags={"@example"})
 public class App {
 	
 }
